6a2412d956e67e560a86c1afc68579c3ef021d45
Jaeha Choi <jaeha220@gmail.com>
2021-07-21 15:15:40 -0700

    Add mutex and fix bug

--- internal/server/server.go
+++ internal/server/server.go
@@ -17,7 +17,9 @@ import (
 )
 
 const (
-	keyPairName    = "server"
+	// Name for the public/private key pair
+	keyPairName = "server"
+	// addCodeArrSize is the max number for Add Code
 	addCodeArrSize = 999999
 )
 
@@ -27,19 +29,22 @@ type Server struct {
 	Port     uint16 `yaml:"port"`
 	CertPath string `yaml:"cert_path"`
 
+	addCodeArrMutex *sync.RWMutex
 	// addCodeArr stores shuffled Add Code in integer with a boolean indicating
 	// the status of the Add Code.
 	// True if available for the server to allocate the Add Code to other device, false otherwise.
 	// 000 000 is reserved for initial value, and should not be used for allocation.
 	// i.e. addCodeArr[index] = [Add Code (int), isUsed (bool)]
-	addCodeArr [addCodeArrSize][2]interface{} // TODO: Add mutex for addCodeArr
+	addCodeArr [addCodeArrSize][2]interface{}
 
 	// addCodeIdx stores indices to actual Add Code (inverse of addCodeArr)
 	// i.e. addCodeIdx[addCode] = index to Add Code in addCodeArr
-	addCodeIdx [addCodeArrSize]int `yaml:"add_code_idx"`
+	// Requires no lock as addCodeIdx is read-only after initAddCode()
+	addCodeIdx [addCodeArrSize]int
 
+	nextAddCodeIdxMutex *sync.Mutex
 	// nextAddCodeIdx represents index of next available Add Code element in addCodeArr
-	nextAddCodeIdx int // TODO: Add mutex for nextAddCodeIdx
+	nextAddCodeIdx int
 
 	// devices store Add Codes as a key (int) and hash of a public key as a value (string).
 	devices sync.Map
@@ -63,6 +68,7 @@ func init() {
 }
 
 func (serv *Server) initAddCode() {
+	// No need to lock here as no other goroutine accesses these arrays yet.
 	for i := 0; i < addCodeArrSize; i++ {
 		serv.addCodeArr[i] = [2]interface{}{i + 1, false}
 	}
@@ -73,47 +79,28 @@ func (serv *Server) initAddCode() {
 	})
 }
 
-func (serv *Server) RemoveDevice(addCode int) {
-	addCodeIdx := serv.addCodeIdx[addCode]
-	serv.addCodeArr[addCodeIdx][1] = false
-	serv.devices.Delete(addCode)
-	//log.Debug("removeDev//addCodeArr: ", serv.addCodeArr[addCodeIdx])
-	//log.Debug(serv.devices.Load(addCode))
+// InitConfig initializes Server struct, Add Codes, and TLS configuration.
+func InitConfig() (serv *Server, err error) {
+	serv = &Server{
+		Host:                "127.0.0.1",
+		Port:                9129,
+		CertPath:            "./data/cert",
+		addCodeArr:          [addCodeArrSize][2]interface{}{},
+		addCodeIdx:          [addCodeArrSize]int{},
+		nextAddCodeIdx:      0,
+		devices:             sync.Map{},
+		tls:                 nil,
+		addCodeArrMutex:     &sync.RWMutex{},
+		nextAddCodeIdxMutex: &sync.Mutex{},
+	}
+	serv.initAddCode()
+	if err := serv.tlsConfig(); err != nil {
+		return nil, err
+	}
+	return serv, nil
 }
 
-func (serv *Server) AddDevice(pubKeyHash string) (addCode int, err error) {
-	retry := 0
-	elem := serv.addCodeArr[serv.nextAddCodeIdx]
-	// Repeat until available code is found.
-	// elem[1] is always boolean; no need to check for error
-	for elem[1].(bool) {
-		serv.nextAddCodeIdx += 1
-		elem = serv.addCodeArr[serv.nextAddCodeIdx]
-		retry += 1
-		// If every possible code is taken, return error
-		if retry == addCodeArrSize {
-			log.Error("Every add code is being used")
-			return -1, NoAvailableAddCodeError
-		}
-	}
-	// Get Add Code
-	// elem[0] is always int; no need to check for error
-	addCode = elem[0].(int)
-	// Mark current Add Code as being used
-	serv.addCodeArr[serv.nextAddCodeIdx][1] = true
-	// Increment next available Add Code index
-	serv.nextAddCodeIdx += 1
-	// if serv.nextAddCodeIdx reaches the max size, reset to 0
-	if serv.nextAddCodeIdx == addCodeArrSize {
-		serv.nextAddCodeIdx = 0
-	}
-	// Add device public key hash to online devices
-	serv.devices.Store(addCode, pubKeyHash)
-	//log.Debug("addDev//addCodeArr: ", serv.addCodeArr[serv.nextAddCodeIdx-1])
-	//log.Debug(serv.devices.Load(addCode))
-	return addCode, nil
-}
-
+// tlsConfig initializes TLS configuration
 func (serv *Server) tlsConfig() (err error) {
 	serv.tls = &tls.Config{
 		Rand:         rand.Reader,
@@ -143,26 +130,13 @@ func (serv *Server) tlsConfig() (err error) {
 	return nil
 }
 
-func InitConfig() (serv *Server, err error) {
-	serv = &Server{
-		Host:           "127.0.0.1",
-		Port:           9129,
-		CertPath:       "./data/cert",
-		addCodeArr:     [addCodeArrSize][2]interface{}{},
-		addCodeIdx:     [addCodeArrSize]int{},
-		nextAddCodeIdx: 0,
-		devices:        sync.Map{},
-		tls:            nil,
-	}
-	serv.initAddCode()
-	if err := serv.tlsConfig(); err != nil {
-		return nil, err
-	}
-	return serv, nil
-}
-
 // ReadConfig reads a config from a yaml file
 func ReadConfig(fileName string) (serv *Server, err error) {
+	serv, err = InitConfig()
+	if err != nil {
+		log.Debug(err)
+		return nil, err
+	}
 	file, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		log.Debug(err)
@@ -175,11 +149,92 @@ func ReadConfig(fileName string) (serv *Server, err error) {
 		log.Error("Error while parsing config.yml")
 		return nil, err
 	}
-	serv.initAddCode()
-	if err := serv.tlsConfig(); err != nil {
-		return nil, err
-	}
 	return serv, nil
+}
+
+// RemoveDevice removes a device with provided Add Code from the server,
+// preventing other devices from searching this device
+func (serv *Server) RemoveDevice(addCode int) {
+	// Find the index of Add Code with the inverse array
+	addCodeIdx := serv.addCodeIdx[addCode-1]
+
+	// Write lock
+	serv.addCodeArrMutex.Lock()
+	// Mark the Add Code as available, by setting isUsed field to false
+	serv.addCodeArr[addCodeIdx][1] = false
+	serv.addCodeArrMutex.Unlock()
+
+	// Remove device from map
+	serv.devices.Delete(addCode)
+
+	// Log
+	//log.Debug("removeDev//addCodeArr: ", serv.addCodeArr[addCodeIdx])
+	//log.Debug(serv.devices.Load(addCode))
+}
+
+// AddDevice adds a device by adding the public key hash to the server,
+// so that other devices can query and request public key.
+// Returns assigned Add Code, where 0 < Add Code <= addCodeArrSize
+// Returns NoAvailableAddCodeError if all Add Code is in use
+func (serv *Server) AddDevice(pubKeyHash string) (addCode int, err error) {
+	// serv.nextAddCodeIdxMutex is locked until the return statement of this function
+	// since it updates indices. May change depending on the significance of throttling.
+	serv.nextAddCodeIdxMutex.Lock()
+	defer serv.nextAddCodeIdxMutex.Unlock()
+
+	// Get next available Add Code.
+	// Users could forget to switch unregister themselves and still occupy the AddCode,
+	// so we need to check if the Add Code is actually available.
+	serv.addCodeArrMutex.RLock()
+	elem := serv.addCodeArr[serv.nextAddCodeIdx]
+	serv.addCodeArrMutex.RUnlock()
+
+	retry := 0
+	// Repeat until available code is found.
+	// elem[1] is always boolean; no need to check for error
+	for elem[1].(bool) {
+		serv.nextAddCodeIdx += 1
+		// if serv.nextAddCodeIdx reaches the max size, reset to 0
+		if serv.nextAddCodeIdx == addCodeArrSize {
+			serv.nextAddCodeIdx = 0
+		}
+
+		// Get next available Add Code.
+		serv.addCodeArrMutex.RLock()
+		elem = serv.addCodeArr[serv.nextAddCodeIdx]
+		serv.addCodeArrMutex.RUnlock()
+
+		retry += 1
+		// If every possible code is taken, return error
+		if retry == addCodeArrSize {
+			log.Error("Every add code is being used")
+			return -1, NoAvailableAddCodeError
+		}
+	}
+	// Get Add Code
+	// elem[0] is always int; no need to check for error
+	addCode = elem[0].(int)
+
+	// Mark current Add Code as being used
+	serv.addCodeArrMutex.Lock()
+	serv.addCodeArr[serv.nextAddCodeIdx][1] = true
+	serv.addCodeArrMutex.Unlock()
+
+	// Increment next available Add Code index
+	serv.nextAddCodeIdx += 1
+
+	// if serv.nextAddCodeIdx reaches the max size, reset to 0
+	if serv.nextAddCodeIdx == addCodeArrSize {
+		serv.nextAddCodeIdx = 0
+	}
+
+	// Add device public key hash to online devices
+	serv.devices.Store(addCode, pubKeyHash)
+
+	// Log
+	//log.Debug("addDev//addCodeArr: ", serv.addCodeArr[serv.nextAddCodeIdx-1])
+	//log.Debug(serv.devices.Load(addCode))
+	return addCode, nil
 }
 
 func connectionHandler(conn net.Conn) (err error) {
